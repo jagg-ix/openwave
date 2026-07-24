dddf3d462f66666ea3fba8f58976b48191273b3d
Jorge A. Garcia <681013+jagg-ix@users.noreply.github.com>
2026-07-24 16:01:44 -0600

    feat(M9): add M9.74 runner

---
+++ openwave/xperiments/m9_cat_ept/research/scripts/m9_74_independent_mode_robustness.py
@@ -0,0 +1,3 @@
+from openwave.xperiments.m9_cat_ept.independent_mode_robustness import result_to_json, run_independent_mode_robustness
+
+print(result_to_json(run_independent_mode_robustness()), end="")
