1fe012963839897f3e315c5268287087fcc2337e
Jorge A. Garcia <681013+jagg-ix@users.noreply.github.com>
2026-07-26 10:13:15 -0600

    feat(M9.107): add coupled-sector runner

---
+++ openwave/xperiments/m9_cat_ept/research/scripts/m9_107_coupled_sector_fields.py
@@ -0,0 +1,2 @@
+from openwave.xperiments.m9_cat_ept.coupled_sector_fields import result_to_json, run_coupled_sector_field_campaigns
+print(result_to_json(run_coupled_sector_field_campaigns()), end="")
