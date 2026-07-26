92b0a80cf391629319eff747e1ea725761b7fa40
Jorge A. Garcia <681013+jagg-ix@users.noreply.github.com>
2026-07-26 17:28:34 -0600

    feat(M9.109): add G universality runner

---
+++ openwave/xperiments/m9_cat_ept/research/scripts/m9_109_G_clock_universality.py
@@ -0,0 +1,3 @@
+from openwave.xperiments.m9_cat_ept.newton_g_clock_universality import result_to_json, run_newton_G_clock_universality
+
+print(result_to_json(run_newton_G_clock_universality()), end="")
