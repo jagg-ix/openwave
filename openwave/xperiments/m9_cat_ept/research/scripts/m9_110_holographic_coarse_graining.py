0cf413e191892ee7c1c4a5e432c299adc4beafa8
Jorge A. Garcia <681013+jagg-ix@users.noreply.github.com>
2026-07-26 21:50:19 -0600

    feat(M9.110): add coarse graining runner

---
+++ openwave/xperiments/m9_cat_ept/research/scripts/m9_110_holographic_coarse_graining.py
@@ -0,0 +1,3 @@
+from openwave.xperiments.m9_cat_ept.holographic_coarse_graining import result_to_json, run_holographic_coarse_graining
+
+print(result_to_json(run_holographic_coarse_graining()), end="")
