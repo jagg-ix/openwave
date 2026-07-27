2077b11bbed1ec5d23f974389d46756a684404f1
Jorge A. Garcia <681013+jagg-ix@users.noreply.github.com>
2026-07-26 23:03:57 -0600

    feat(M9.113): add synchronized evolution runner

---
+++ openwave/xperiments/m9_cat_ept/research/scripts/m9_113_synchronized_screen_gravity.py
@@ -0,0 +1,3 @@
+from openwave.xperiments.m9_cat_ept.synchronized_screen_gravity_evolution import result_to_json, run_synchronized_screen_gravity_evolution
+
+print(result_to_json(run_synchronized_screen_gravity_evolution()), end="")
